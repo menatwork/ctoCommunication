d848c02941eac92772d001d7a5b10de11b83911c
Stefan heimes <stefan_heimes@hotmail.com>
2012-03-22 16:48:57 +0100

    Language update

--- system/modules/ctoCommunication/languages/de/tl_settings.php
+++ system/modules/ctoCommunication/languages/de/tl_settings.php
@@ -36,7 +36,7 @@ $GLOBALS['TL_LANG']['tl_settings']['ctoCommunication_legend']       = 'ctoCommun
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_settings']['ctoCom_APIKey']                 = array('ctoCommunication API Key', 'Dieser Schlüssel sichert die Kommunikation zwischen den Contao-Installationen ab.');
-$GLOBALS['TL_LANG']['tl_settings']['ctoCom_responseLength']         = array('Maximale Zeichenlänge einer HTTP-Anfrage', 'Hier können Sie die maximale Länge einer HTTP-Anfrage verändern. Standardmäßig sind es 40.000 Zeichen.');
+$GLOBALS['TL_LANG']['tl_settings']['ctoCom_responseLength']         = array('Maximale Zeichenlänge einer HTTP-Anfrage', 'Hier können Sie die maximale Länge einer HTTP-Anfrage verändern. Standardmäßig sind es 135.000.000 Zeichen.');
 $GLOBALS['TL_LANG']['tl_settings']['handshake']                     = array('Handshake-Funktion deaktivieren', 'Die Synchronisation ohne einen Drei-Wege-Handschlag absichern.');
 
 
--- system/modules/ctoCommunication/languages/en/tl_settings.php
+++ system/modules/ctoCommunication/languages/en/tl_settings.php
@@ -36,7 +36,7 @@ $GLOBALS['TL_LANG']['tl_settings']['ctoCommunication_legend']       = 'ctoCommun
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_settings']['ctoCom_APIKey']                 = array('ctoCommunication API Key', 'This key ensures the communication between the contao installations.');
-$GLOBALS['TL_LANG']['tl_settings']['ctoCom_responseLength']         = array('Maximum request length', 'Here you can change the maximum length of HTTP requests. By default 40,000 characters.');
+$GLOBALS['TL_LANG']['tl_settings']['ctoCom_responseLength']         = array('Maximum request length', 'Here you can change the maximum length of HTTP requests. By default 135,000,000 characters.');
 $GLOBALS['TL_LANG']['tl_settings']['handshake']                     = array('Deactivate handshake function', 'Secure the synchronization without the three-way handshake.');
 
 ?>
