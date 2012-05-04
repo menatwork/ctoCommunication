3791b7dd2f8f93e8f2ebe3ab23dfd6434e083feb
Stefan heimes <stefan_heimes@hotmail.com>
2012-05-04 16:32:24 +0200

    Improves the exception handling

--- system/modules/ctoCommunication/CtoCommunication.php
+++ system/modules/ctoCommunication/CtoCommunication.php
@@ -76,49 +76,6 @@ class CtoCommunication extends Backend
      */
     protected $intMaxResponseLength;
     protected $intHandshakeTimeout = 1200;
-    protected $arrResponses        = array(
-        100 => 'Continue',
-        101 => 'Switching Protocols',
-        200 => 'OK',
-        201 => 'Created',
-        202 => 'Accepted',
-        203 => 'Non-Authoritative Information',
-        204 => 'No Content',
-        205 => 'Reset Content',
-        206 => 'Partial Content',
-        207 => 'Multi-Status',
-        300 => 'Multiple Choices',
-        301 => 'Moved Permanently',
-        302 => 'Found',
-        303 => 'See Other',
-        304 => 'Not Modified',
-        305 => 'Use Proxy',
-        307 => 'Temporary Redirect',
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        407 => 'Proxy Authentication Required',
-        408 => 'Request Timeout',
-        409 => 'Conflict',
-        410 => 'Gone',
-        411 => 'Length Required',
-        412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Large',
-        415 => 'Unsupported Media Type',
-        416 => 'Requested Range Not Satisfiable',
-        417 => 'Expectation Failed',
-        500 => 'Internal Server Error',
-        501 => 'Not Implemented',
-        502 => 'Bad Gateway',
-        503 => 'Service Unavailable',
-        504 => 'Gateway Timeout',
-        505 => 'HTTP Version Not Supported'
-    );
 
     /**
      * @var CtoComCodifyengineAbstract 
@@ -688,15 +645,20 @@ class CtoCommunication extends Backend
             $objRequest->datamime = $objMultipartFormdata->getContentTypeHeader();
         }
 
+        $booRequestResult = $objRequest->send($this->strUrl . $this->strUrlGet);
+        
         // Send new request
-        $objRequest->send($this->strUrl . $this->strUrlGet);
+        if($booRequestResult == false || $objRequest->hasError())
+        {
+            throw new Exception("Error on transmission, with message: " . $objRequest->code . " " . $objRequest->error);
+        }
 
         $this->objDebug->addDebug("Request", substr($objRequest->request, 0, 2000));
 
         /* ---------------------------------------------------------------------
          * Check response for errors
          */
-
+        
         // Build response Header informations
         $strResponseHeader = "";
         foreach ($objRequest->headers as $keyHeader => $valueHeader)
@@ -710,26 +672,6 @@ class CtoCommunication extends Backend
         if ($objRequest->timedOut)
         {
             throw new Exception("Sorry we have a time out. Please try again.");
-        }
-
-        // Check if everything is okay for connection
-        if ($objRequest->hasError())
-        {
-            throw new Exception("Error on transmission, with message: " . $objRequest->code . " " . $objRequest->error);
-        }
-
-        // Check if everything is okay for connection
-        if ($objRequest->code != 200)
-        {
-            // Check if we have time out
-            if (empty($objRequest->code))
-            {
-                throw new Exception("Sorry we have a time out. Please try again.");
-            }
-            else
-            {
-                throw new Exception("Error on transmission, with message: " . $objRequest->code . " - " . $this->arrResponses[$objRequest->code]);
-            }
         }
 
         // Check if we have a response
